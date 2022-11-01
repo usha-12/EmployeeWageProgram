02b923f346bb639cb43239a12af4a8f283d49f0d
usha-12 <usha12ahirwar@gmail.com>
2022-11-01 11:31:28 +0530

    Daily employee wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,16 +1,23 @@
 package com.bridgelabz.employeewage;
 public class EmployeeWage {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
+
 
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME)
-            System.out.println("Employee is Present");
+            empHrs = 8;
         else
-            System.out.println("Employee is Absent");
+            empHrs = 0;
+
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+
+        System.out.println("Employee Daily Wage : $" + empWage);
     }
-    }
-    /*Check Employee is
-Present or Absent
-- Use ((RANDOM)) for Attendance
-Check*/
+}
+/*Calculate Daily
+Employee Wage*/
