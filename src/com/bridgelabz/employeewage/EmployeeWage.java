2eec2a03993e77e7398f2017dfdf4faa969e097c
usha-12 <usha12ahirwar@gmail.com>
2022-11-01 12:11:33 +0530

    Calculate wages till a condition of total working hourse or days

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,17 +1,22 @@
 package com.bridgelabz.employeewage;
+
 public class EmployeeWage {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
 
     public static void main(String args[]) {
 
         int empHrs = 0;
         int empWage = 0;
         int totalEmpWage = 0;
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0;
 
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+
+        for (totalWorkingDays = 1; totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS; totalWorkingDays++) {
 
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
@@ -33,15 +38,17 @@ public class EmployeeWage {
             }
 
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            System.out.println("Employee Daily Wage : $" + empWage);
+            totalEmpHrs += empHrs;
+            System.out.println("Day " + totalWorkingDays + " Employee hours : " + empHrs + " Wage $" + empWage);
         }
 
+        totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println();
         System.out.println("Employee Monthly Wage : $" + totalEmpWage);
-
     }
 }
-/*Calculating Wages
-for a Month
-- Assume 20 Working Day per Month*/
+/*Calculate Wages till
+a condition of total
+working hours or
+days is reached for
+a month - Assume 100 hours and 20 days*/
