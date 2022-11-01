df85d3cf80438bcc2f06c120d0c0ebff097107c7
usha-12 <usha12ahirwar@gmail.com>
2022-11-01 13:17:02 +0530

    Store the Daily wage along with the Total wage

--- src/com/bridgelabz/employeewage/CompanyWage.java
+++ src/com/bridgelabz/employeewage/CompanyWage.java
@@ -5,17 +5,21 @@ public class CompanyWage {
     public final int numOfWorkingDays;
     public final int maxHoursInMonth;
     public int totalEmpWage;
+
     public CompanyWage(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth) {
         this.companyName = companyName;
         this.empRatePerHour = empRatePerHour;
         this.numOfWorkingDays = numOfWorkingDays;
         this.maxHoursInMonth = maxHoursInMonth;
+        totalEmpWage = 0;
     }
+
     public void setTotalEmpWage(int totalEmpWage) {
         this.totalEmpWage = totalEmpWage;
     }
+
     @Override
     public String toString() {
-        return "\n"+companyName+" company Employee Monthly Wage : $" + totalEmpWage+"\n\n";
+        return "\n" + companyName + " company Employee Monthly Wage : $" + totalEmpWage + "\n\n";
     }
 }
