2c20d5813adbe56b4ffd370d3905ad4e1940c2c7
usha-12 <usha12ahirwar@gmail.com>
2022-11-01 12:53:50 +0530

    ability to manage emp wage of multiple companies using interface

--- src/com/bridgelabz/employeewage/CompanyWage.java
+++ src/com/bridgelabz/employeewage/CompanyWage.java
@@ -1,24 +1,19 @@
 package com.bridgelabz.employeewage;
-
 public class CompanyWage {
     public final String companyName;
     public final int empRatePerHour;
     public final int numOfWorkingDays;
     public final int maxHoursInMonth;
     public int totalEmpWage;
-
     public CompanyWage(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth) {
         this.companyName = companyName;
         this.empRatePerHour = empRatePerHour;
         this.numOfWorkingDays = numOfWorkingDays;
         this.maxHoursInMonth = maxHoursInMonth;
     }
-
-
     public void setTotalEmpWage(int totalEmpWage) {
         this.totalEmpWage = totalEmpWage;
     }
-
     @Override
     public String toString() {
         return "\n"+companyName+" company Employee Monthly Wage : $" + totalEmpWage+"\n\n";
